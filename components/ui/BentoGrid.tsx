5692e7cf6978b661dce25aeaa0e19d6df2efd9fb
bluebeard <itzshravya07@gmail.com>
2025-12-03 01:11:26 +0530

    Refactor BentoGrid component to dynamically import Lottie for improved performance

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import { IoCopyOutline } from "react-icons/io5";
 
 // Also install this npm i --save-dev @types/react-lottie
-import Lottie from "react-lottie";
+// import Lottie from "react-lottie";
 
 import { cn } from "@/lib/utils";
 
@@ -11,6 +11,12 @@ import { BackgroundGradientAnimation } from "./GradientBg";
 import GridGlobe from "./GridGlobe";
 import animationData from "@/data/confetti.json";
 import MagicButton from "../MagicButton";
+import dynamic from "next/dynamic";
+
+// ADD THIS BLOCK
+const Lottie = dynamic(() => import("react-lottie"), {
+  ssr: false,
+});
 
 export const BentoGrid = ({
   className,
